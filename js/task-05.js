fdf98ce06ff7926b18ea4c0b612e12dedfaf77cc
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-05 20:33:28 +0300

    Done task-05

---
+++ js/task-05.js
@@ -0,0 +1,19 @@
+const refs = {
+    input: document.querySelector('#name-input'),
+    nameLabel: document.querySelector('#name-output'),
+};
+
+refs.input.addEventListener('input', onInput);
+console.log(refs.nameLabel.textContent);
+
+
+
+function onInput(event) {
+    // console.log('Инпут получил фокус - событие input');
+    // console.log(event.currentTarget.value);
+    // console.log(refs.nameLabel.textContent);
+    if (event.currentTarget.value === "") 
+        refs.nameLabel.textContent = "Anonymous"
+        else refs.nameLabel.textContent = event.currentTarget.value;
+}
+
