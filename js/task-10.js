2a8bd01543fe67708c4be453e30c61659efc4c08
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-06 00:56:12 +0300

    Started task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+function createBoxes(amount) {
+
+}
+
+
+const refs = {
+    div: document.querySelector('#controls'),
+    buttonCreate: document.querySelector('[data-create]'),
+    buttonDestroy: document.querySelector('[data-destroy]'),
+};
+
+const input = refs.div.firstElementChild;
+// console.log(input);
+
+input.addEventListener('click', event => {
+  // console.log("Вешаю слушателя события на нопку buttonChangeColor");
+  const amount = input.value
+  console.log("amount:", amount);
+
+  const randomColor = getRandomHexColor();
+  // console.log(randomColor);
+
+
+});
