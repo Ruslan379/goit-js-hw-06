84b79408f3532af3abe97b3b2bae168e24404375
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-05 16:22:27 +0300

    Done task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,39 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listCategories = document.querySelector('#ingredients')
+// console.log(listCategories);
+
+// const l = ingredients.length
+// console.log(l);
+
+// const elLi = document.createElement("li");
+// console.log(elLi);
+
+// elLi.textContent = "Potatoes";
+// console.log(elLi.textContent);
+// console.log(elLi);
+
+//! 1-ый вариант: (добавляет в DOM каждый элемент <li> в цикле на каждой итерации)
+// for (let i = 0; i < ingredients.length; i+=1) {
+//   const elLi = document.createElement("li");
+//   elLi.textContent = ingredients[i];
+//   elLi.classList.add("item");
+//   listCategories.appendChild(elLi) 
+// };
+//!___________________________________________________
+
+//! 2-ый вариант (добавляет в DOM все элементы <li> за один раз):
+const arrLi = []
+
+for (let i = 0; i < ingredients.length; i+=1) {
+  const elLi = document.createElement("li");
+  elLi.textContent = ingredients[i];
+  elLi.classList.add("item");
+  arrLi.push(elLi);
+};
+
+listCategories.append(...arrLi);
+// console.log(listCategories);
+  //!___________________________________________________
