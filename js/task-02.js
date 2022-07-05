bc2c254423b49c24833058edf8eb6f286d0113ff
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-05 17:04:29 +0300

    Done task-02 (4 var.)

--- js/task-02.js
+++ js/task-02.js
@@ -20,25 +20,69 @@ const listCategories = document.querySelector('#ingredients')
 // console.log(elLi.textContent);
 // console.log(elLi);
 
-//! 1-ый вариант: (добавляет в DOM каждый элемент <li> в цикле на каждой итерации)
+//TODO 1-ый вариант: (добавляет в DOM каждый элемент <li> в цикле на каждой итерации)
 // for (let i = 0; i < ingredients.length; i+=1) {
 //   const elLi = document.createElement("li");
 //   elLi.textContent = ingredients[i];
 //   elLi.classList.add("item");
 //   listCategories.appendChild(elLi) 
 // };
-//!___________________________________________________
+//TODO___________________________________________________
 
-//! 2-ый вариант (добавляет в DOM все элементы <li> за один раз):
-const arrLi = []
 
-for (let i = 0; i < ingredients.length; i+=1) {
-  const elLi = document.createElement("li");
-  elLi.textContent = ingredients[i];
-  elLi.classList.add("item");
-  arrLi.push(elLi);
-};
 
+
+  //TODO 2-ый вариант (добавляет в DOM все элементы <li> за один раз):
+// const arrLi = []
+
+// for (let i = 0; i < ingredients.length; i+=1) {
+//   const elLi = document.createElement("li");
+//   elLi.textContent = ingredients[i];
+//   elLi.classList.add("item");
+//   arrLi.push(elLi);
+// }; 
+
+// // console.log(arrLi);
+// listCategories.append(...arrLi);
+// // console.log(listCategories);
+  //TODO___________________________________________________
+
+
+
+//TODO 3-ый вариант (создает новый массив элементов <li> с помощью .map и добавляет в DOM все элементы <li> за один раз):
+// const arrLi = ingredients.map(ingredient => {
+//   // console.log(ingredient);
+//   const elLi = document.createElement("li");
+//   elLi.textContent = ingredient;
+//   elLi.classList.add("item");
+//   return elLi;
+// });
+
+// // console.log(arrLi);
+// listCategories.append(...arrLi);
+// // console.log(listCategories);
+  //TODO___________________________________________________
+
+
+
+//!         4-ый вариант:
+//!   (создает новый массив элементов <li> 
+//!   с помощью ф - ции и .map + колбэк => 
+//!   и добавляет в DOM все элементы <li> за один раз):
+
+const addedLiOnList = arr => {
+  return arr.map(ingredient => {
+    // console.log(ingredient);
+    const elLi = document.createElement("li");
+    elLi.textContent = ingredient;
+    elLi.classList.add("item");
+    return elLi;
+  });
+}
+
+const arrLi = addedLiOnList(ingredients); 
+
+// console.log(arrLi);
 listCategories.append(...arrLi);
 // console.log(listCategories);
   //!___________________________________________________
