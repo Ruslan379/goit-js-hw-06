298f5f7a8534121391841401d99c5911db58ade8
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-06 00:16:52 +0300

    Done task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,26 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+    buttonChangeColor: document.querySelector('.change-color'),
+    colorValue: document.querySelector('.color'),
+    body: document.body,
+};
+
+// console.log(refs.colorValue.textContent);
+
+
+refs.buttonChangeColor.addEventListener('click', event => {
+  // console.log("Вешаю слушателя события на нопку buttonChangeColor");
+
+  const bodyColor = getRandomHexColor();
+  // console.log(bodyColor);
+
+  refs.body.style.backgroundColor = bodyColor;
+
+  refs.colorValue.textContent = bodyColor;
+
+});
+
+// console.log(getRandomHexColor());
